6b4bd113eee4e1cb4eff06d8566eadd1a760c311
jonathan <you@example.com>
2021-11-28 14:11:13 -0600

    created Pizza and Toppings models

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
+class Pizza(models.Model):
+    names = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.names
+
+
+class Toppings(models.Model):
+    types = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.types
+
 # Create your models here.
