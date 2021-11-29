cd8a3a683638cf5342f26d029a92939511403135
jonathan <you@example.com>
2021-11-29 14:13:27 -0600

    added Classes to Admin.py

--- MainApp/admin.py
+++ MainApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Pizza
+admin.site.register(Pizza)
+
+from .models import Toppings
+admin.site.register(Toppings)
+
