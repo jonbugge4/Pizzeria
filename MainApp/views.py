8722f6ae343c6d3c5977f181e3e3edd51a925464
jonathan <you@example.com>
2021-12-01 13:40:40 -0600

    adding to Views

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+
+from .models import Pizza
+
+
 # Create your views here.
+
+def index(request): 
+    return render(request, 'MainApp/home.html')
+
+def pizza(request):
+    pizza = Pizza.objects.order_by('date_added')
+
+    context = {'pizza': pizza}
+
+    return render(request, 'MainApp/topics.html', context)
