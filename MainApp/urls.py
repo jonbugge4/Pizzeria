f2997acb48c9e7238e7bfb0d974b9eb939d7710e
jon <jonathan_bugge1@baylor.edu>
2021-12-10 15:14:27 -0600

    need to get pizzas to show

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -8,5 +8,8 @@ app_name = 'MainApp'
 urlpatterns = [
     path('',views.index, name='index'),
     path('pizzas',views.pizzas, name='pizzas'),
-    path('pizzas/<int:pizza_id>/', views.pizzas, name='pizzas')
+    path('pizzas/<int:pizza_id>/', views.pizzas, name='pizzas'),
+    path('pizza',views.pizza, name='pizza'),
+    path('new_comment',views.new_comment, name='new_comment')
+
 ]
