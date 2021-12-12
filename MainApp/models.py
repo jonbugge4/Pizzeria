dfd93782453c53e01ede0460237cb2076148eb8b
jonathan <you@example.com>
2021-12-11 22:32:45 -0600

    adding pictures

--- MainApp/models.py
+++ MainApp/models.py
@@ -4,7 +4,9 @@ from django.db import models
 class Pizza(models.Model):
     names = models.CharField(max_length=20)
     date_added = models.DateTimeField(auto_now_add=True)
-
+    header_image = models.ImageField(
+        null=True, blank=True, upload_to='images/')
+        
     def __str__(self):
         return self.names
 
